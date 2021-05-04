c32480db3e397bdec378772ef382e56f25902c01
ravinalawade <ravinalawade5@gmail.com>
2021-05-04 18:47:26 +0530

    new error6

--- web_project/settings.py
+++ web_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+import django_heroku
 
 # Build paths 
 BASE_DIR = Path(__file__).resolve().parent.parent
